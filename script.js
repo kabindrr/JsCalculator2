eca9c2a3c8bea8046e1de79f4d54859a8ebcc57a
kabindrr <chaina_kabindra@yahoo.com>
2024-08-24 11:47:33 +1000

    jscalculator second display

---
+++ script.js
@@ -0,0 +1,39 @@
+const allButtonsElement = document.querySelectorAll(".btn");
+
+let strToDisplay = "";
+const displayElm = document.querySelector(".display");
+const displayElm1 = document.querySelector(".display1");
+
+const buttonAction = (value) => {
+  if (value === "AC") {
+    strToDisplay = "";
+    displayElm1.innerText = "" || 0;
+    return display(strToDisplay);
+  }
+  if (value === "=") {
+    return totalCalculation();
+  }
+  strToDisplay += value;
+  display(strToDisplay);
+  displayElm1.innerText = strToDisplay;
+};
+
+allButtonsElement.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const value = btn.innerText;
+    buttonAction(value);
+  });
+});
+const display = (str) => {
+  displayElm.innerText = str || "0.0";
+};
+
+//calculate total calculations
+const totalCalculation = () => {
+  try {
+    const total = eval(strToDisplay);
+    display(total);
+  } catch (error) {
+    display("Error");
+  }
+};
