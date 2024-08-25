ba62ffb121fc531ae2e5403dd2da99343f4d1a3c
kabindrr <chaina_kabindra@yahoo.com>
2024-08-25 11:51:53 +1000

    make C button work

--- script.js
+++ script.js
@@ -4,26 +4,33 @@ let strToDisplay = "";
 const displayElm = document.querySelector(".display");
 const displayElm1 = document.querySelector(".display1");
 
-const buttonAction = (value) => {
-  if (value === "AC") {
-    strToDisplay = "";
-    displayElm1.innerText = "";
-    return display(strToDisplay);
-  }
-  if (value === "=") {
-    return totalCalculation();
-  }
-  strToDisplay += value;
-  display(strToDisplay);
-  displayElm1.innerText = strToDisplay;
-};
-
 allButtonsElement.forEach((btn) => {
   btn.addEventListener("click", () => {
     const value = btn.innerText;
     buttonAction(value);
   });
 });
+
+const buttonAction = (value) => {
+  if (value === "AC") {
+    strToDisplay = "";
+    displayElm1.innerText = "";
+    return display(strToDisplay);
+  }
+  if (value === "C") {
+    strToDisplay = strToDisplay.slice(0, -1);
+    displayElm1.innerText = displayElm1.innerText.slice(0, -1);
+    return display(strToDisplay);
+  }
+  if (value === "=") {
+    return totalCalculation();
+  }
+
+  strToDisplay += value;
+  display(strToDisplay);
+  displayElm1.innerText = strToDisplay;
+};
+
 const display = (str) => {
   displayElm.innerText = str || "0.0";
 };
@@ -33,7 +40,7 @@ const totalCalculation = () => {
   try {
     const total = eval(strToDisplay);
     display(total);
-    strToDisplay = total;
+    strToDisplay = total.toString();
   } catch (error) {
     display("Error");
     displayElm1.innerText = "";
